70848daba4cd599634f6a17fab4fd4fa3f0cc97b
Luca <mylince@gmail.com>
2020-07-29 01:05:41 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,32 @@
+// Ajax call
+
+const apiUrl = "https://sv443.net/jokeapi/v2/joke/Programming";
+
+$.ajax({
+    url: apiUrl,
+    method: 'GET',
+    success: function(data){
+            var setup = data.setup;
+            var delivery = data.delivery;
+            var joke = data.joke;
+            $('.delivery h4').html(setup);
+            $('.left p').html("&#8246;" + delivery + "&#8243;");
+            $('.joke p').html(joke);
+            // Different View between questions and joke
+            if (joke == null) {
+                $('.delivery').show();
+            } else {
+                $('.joke').show();
+            }
+        },
+        error: function(){
+            alert('errore')
+        }
+});
+
+
+// Show button
+$('.delivery').on('click', '.show-this-button', function(){
+    $('.left p').show();
+    $('.delivery h2').hide();
+})
